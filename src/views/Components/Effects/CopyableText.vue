70c163c9c63a5749f9e9ace01db43cc9d5220e12
Stefan Genov <stefantigro@gmail.com>
2021-06-09 09:15:58 +0300

    [* master] Ansible Deploy Version v1.8.0

--- src/views/Components/Effects/CopyableText.vue
+++ src/views/Components/Effects/CopyableText.vue
@@ -1,9 +1,9 @@
 <template>
-	<span :ref="refName" @click="copy" class="cursor-pointer" :class="textColor">
+	<span :ref="refName" @click="copy" class="cursor-pointer" :class="`${textColor} ${mouseDown ? 'opacity-50' : ''}`" @mousedown="mouseDown = true" @mouseup="mouseDown = false">
 		{{ text }}
 		<span class="inline-block my-auto">
-			<svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-				<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2" />
+			<svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+				<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2"/>
 			</svg>
 		</span>
 	</span>
@@ -15,7 +15,8 @@ export default {
 	data: function ()
 	{
 		return {
-			refName: `copyText${Math.random()}`
+			refName		: `copyText${Math.random()}`,
+			mouseDown	: false
 		};
 	},
 	methods: {
