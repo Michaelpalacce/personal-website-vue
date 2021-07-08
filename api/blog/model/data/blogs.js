6211fb64cfba4d5bf67a5e2668f383df02a30aee
Stefan Genov <stefantigro@gmail.com>
2021-07-08 22:15:24 +0300

    [master] Removed a dot

--- api/blog/model/data/blogs.js
+++ api/blog/model/data/blogs.js
@@ -3,7 +3,7 @@ const path	= require( 'path' );
 
 module.exports	= [
 	{
-		title	: 'EventRequest a 0 dependency all-in-one framework, designed for speed.',
+		title	: 'EventRequest a 0 dependency all-in-one framework, designed for speed',
 		date	: '28/6/2021',
 		content	: fs.readFileSync( path.join( __dirname, './blogs/event_request_intro.html' ) )
 	}
