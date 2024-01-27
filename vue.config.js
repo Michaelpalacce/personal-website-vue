fdd8ba4498b27239ae975268d594308e97684591
Stefan Genov <stefan.genov@broadcom.com>
2024-01-27 22:14:50 +0200

    feat: Hopefully updated dependencies?

--- vue.config.js
+++ vue.config.js
@@ -1,22 +1,6 @@
 const config	= {
 	configureWebpack: {
 		devServer: {
-			stats: {
-				colors: true,
-				hash: false,
-				version: false,
-				timings: false,
-				assets: false,
-				chunks: false,
-				modules: false,
-				reasons: false,
-				children: false,
-				source: false,
-				errors: false,
-				errorDetails: false,
-				warnings: false,
-				publicPath: false
-			}
 		}
 	},
 	productionSourceMap: false,
